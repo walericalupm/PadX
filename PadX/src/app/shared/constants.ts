c39eecc212cef83b09dabf935e1c0b5c8e0cce6c
walericalupm <e.aldas@alumnos.upm.es>
2019-12-02 11:58:27 +0100

    Added User Service

--- PadX/src/app/shared/constants.ts
+++ PadX/src/app/shared/constants.ts
@@ -1,5 +1,12 @@
 class Constants {
-  readonly BASE_URI = 'http://fenw.etsisi.upm.es:10000';
-  readonly USER_URI = '/users';
-  readonly USER_LOGIN_URI = '/login';
+  static readonly CONTENT_TYPE_HEADER_KEY = 'Content-Type';
+  static readonly CONTENT_TYPE_JSON_VALUE = 'application/json';
+  static readonly BASE_URI = 'http://fenw.etsisi.upm.es:10000';
+  static readonly USER_URI = '/users';
+  static readonly USER_LOGIN_URI = '/login';
+  static readonly USER_LOGIN_URI_PARAM = 'username=';
+  static readonly PASSWORD_LOGIN_URI_PARAM = 'password=';
+  static readonly CONCAT_PARAM_URI_CHARACTER = '&';
+  static readonly INIT_PARAMS_URI_CHARACTER = '?';
+  static readonly BACKSLASH_URI_CHARACTER = '/';
 }
--- PadX/src/app/shared/services/user-rest.service.ts
+++ PadX/src/app/shared/services/user-rest.service.ts
@@ -1,16 +1,49 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { User } from '../models/user.model';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {User} from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserRestService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
-  /*getLogin(username: string, password: string) {
-     const url = Constants.;
-  }*/
+  getLogin(username: string, password: string) {
+    const url = Constants.BASE_URI.concat(
+      Constants.USER_URI,
+      Constants.USER_LOGIN_URI,
+      Constants.INIT_PARAMS_URI_CHARACTER,
+      Constants.USER_LOGIN_URI_PARAM,
+      username,
+      Constants.CONCAT_PARAM_URI_CHARACTER,
+      Constants.PASSWORD_LOGIN_URI_PARAM,
+      password);
+    return this.http.get(url);
+  }
+
+  getFindUser(username: string) {
+    const url = Constants.BASE_URI.concat(
+      Constants.USER_URI,
+      Constants.BACKSLASH_URI_CHARACTER,
+    username
+  );
+    return this.http.get(url);
+  }
+
+  postUser(user: User) {
+    const headers = new HttpHeaders().set(Constants.CONTENT_TYPE_HEADER_KEY, Constants.CONTENT_TYPE_JSON_VALUE);
+    const url = Constants.BASE_URI.concat(Constants.USER_URI);
+    return this.http.post(url, user, {headers});
+  }
+
+  deleteUser(username: string) {
+    const url = Constants.BASE_URI.concat(
+      Constants.USER_URI,
+      Constants.BACKSLASH_URI_CHARACTER,
+      username);
+    return this.http.delete(url);
+  }
 }
 
 
