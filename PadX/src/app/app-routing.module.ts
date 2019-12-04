278c466310276fc0c4643e3a71fd264160d0f07c
walericalupm <e.aldas@alumnos.upm.es>
2019-12-04 01:45:46 +0100

    Added Login Page | Main Structure developed

--- PadX/src/app/app-routing.module.ts
+++ PadX/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {LoginComponent} from './login/login.component';
+import {HeaderComponent} from './header/header.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: HeaderComponent},
+  {path: 'login', component: LoginComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
