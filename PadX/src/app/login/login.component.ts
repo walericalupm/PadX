7ea32ece5857975f397c94a1680e528e377b3be8
walericalupm <e.aldas@alumnos.upm.es>
2019-12-06 18:29:32 +0100

    Works on Login feature

--- PadX/src/app/login/login.component.ts
+++ PadX/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../shared/models/user.model';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  constructor() {
+    this.user = new User();
+  }
 
   ngOnInit() {
   }
