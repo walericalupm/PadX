0099cd04b7822e445111e1d87f5f72f0afae65c3
walericalupm <e.aldas@alumnos.upm.es>
2019-12-10 01:01:53 +0100

    Token Interceptor added

---
+++ PadX/src/app/shared/intreceptor/authorization-interceptor.ts
@@ -0,0 +1,21 @@
+import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {TokenService} from '../services/token.service';
+import {Observable} from 'rxjs';
+import {Constants} from '../constants';
+
+@Injectable()
+export class AuthorizationInterceptor implements HttpInterceptor{
+  constructor(private tokenService: TokenService){}
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Get Token and Inject It
+    if (this.tokenService.getToken()) {
+      const token = this.tokenService.getToken();
+      const request = req.clone({headers: req.headers.set(
+        Constants.AUTORIZATION_HEADER_KEY,
+          token)});
+      return next.handle(request);
+    }
+  }
+
+}
